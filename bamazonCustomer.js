c8adb2980043e4c10a0066c115280671e4f6a5aa
ashdonnelly <ashdonnelly>
2017-12-16 20:53:19 -0700

    item ordered populating; others are undefined not err

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,26 +3,30 @@ var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Taterbug1!",
-    database: "bamazon_db"
-  });10
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Taterbug1!",
+  database: "bamazon_db"
+});10
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-    if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    console.log("Connected as id: " + connection.threadId);
-    start();
-  });
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  console.log("Connected as id: " + connection.threadId);
+  console.log();
+  console.log("Welcome to BAMAZON!");
+  console.log();
+  start();
+});
 
 // function to prompt user what item they would like to order, query database to get items
 function start() {
+  var itemOrdered;
+  var orderQuantity;
   connection.query("SELECT * FROM products", function(err, data) {
     if (err) throw err;
-    console.log("Welcome to BAMAZON!");
     inquirer
       .prompt([
         {
@@ -36,17 +40,12 @@ function start() {
             return choiceArray;
           },
           message: "Enter the ID of the item you want to order.",
-          validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          }
         },
         {
           name: "orderQuantity",
           type: "input",
           message: "Order quantity:",
+          filter: Number,
           validate: function(value) {
             if (isNaN(value) === false) {
               return true;
@@ -55,49 +54,54 @@ function start() {
           }
         }
       ])
-      .then(function(answer) {
-        // get the information of the chosen item and amount
-        var chosenItem;
-        for (var i = 0; i < data.length; i++) {
-          if (data[i].product_name === itemOrdered) {
-            chosenItem = data[i].product_name;
-          }
-          console.log("Chosen Item: " + chosenItem);
-          console.log("Order Quantity: " + orderQuantity.answer);
-          console.log("Stock Quantity: " + chosenItem.stock_quantity)
-        }
-        // determine if there are enough in stock
-        if (chosenItem.stock_quantity <= parseInt(answer.orderQuantity)) {
-          // stock level was high enough, so update db, let the user know, and start over
-          connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-              {
-                stock_quantity: stock_quantity
-              },
-              {
-                id: chosenItem.id
+      .then(function(userInput) {
+        // get the information of the requested item and amount
+        var requestedItem = userInput.itemOrdered;
+        var requestedQuantity = userInput.Number;
+
+        connection.query("SELECT * FROM products", {itemOrdered: userInput}, function(err, data) {
+          if (err) throw err;
+          
+          console.log("Chosen Item: " + requestedItem);
+          console.log("Order Quantity: " + requestedQuantity);
+          console.log("Stock Quantity: " + requestedItem.stock_quantity)
+          // determine if there are enough in stock
+          if (requestedItem.stock_quantity <= parseInt(userInput.orderQuantity)) {
+            // stock level was high enough, so update db, let the user know, and start over
+            connection.query(
+              "UPDATE products SET ? WHERE ?",
+              [
+                {
+                  stock_quantity: stock_quantity
+                },
+                {
+                  id: requestedItem.id
+                }
+              ],
+              function(err) {
+                if (err) throw err;
+              console.log();
+              console.log("SUCCESS!")
+              console.log();
+              calculateTotal();
+              // calculateTotal();
               }
-            ],
-            function(err) {
-              if (err) throw err;
-            console.log("SUCCESS!")
-            calculateTotal();
-            // calculateTotal();
-            }
-          );
-        } else {
-          // inventory wasn't high enough, start over
-          console.log("INSUFFICIENT INVENTORY!");
-          start();
-        };
+            );
+          } else {
+            // inventory wasn't high enough, start over
+            console.log();
+            console.log("INSUFFICIENT INVENTORY!");
+            console.log();
+            start();
+          };
+        });
       });    
   });
 };
 
 function calculateTotal() {
   // get total price; purchase quantity * item amount
-  var totalPrice = orderQuantity + chosenItem.price;
+  var totalPrice = orderQuantity + requestedItem.price;
   console.log("Your total price is: $" + totalPrice + ". Thank you for using BAMAZON!");
   console.log()
   // ask if they have another purchase and start over; or thank and end
